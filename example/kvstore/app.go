d8ed1866c69fd4e178b9140353bbf1694a8cde93
little51 <little51@126.com>
2020-03-05 13:16:41 +0800

    add comment

--- example/kvstore/app.go
+++ example/kvstore/app.go
@@ -7,7 +7,7 @@ import (
 	abcitypes "github.com/tendermint/tendermint/abci/types"
 )
 
-// KVStoreApplication .
+// KVStoreApplication 数据库变量.
 type KVStoreApplication struct {
 	db           *badger.DB
 	currentBatch *badger.Txn
@@ -15,7 +15,7 @@ type KVStoreApplication struct {
 
 var _ abcitypes.Application = (*KVStoreApplication)(nil)
 
-// NewKVStoreApplication .
+// NewKVStoreApplication KVStoreApplication构造函数，db变量由main.go传入 .
 func NewKVStoreApplication(db *badger.DB) *KVStoreApplication {
 	return &KVStoreApplication{
 		db: db,
@@ -32,6 +32,7 @@ func (KVStoreApplication) SetOption(req abcitypes.RequestSetOption) abcitypes.Re
 	return abcitypes.ResponseSetOption{}
 }
 
+// isValid 校验交易串是否合法
 func (app *KVStoreApplication) isValid(tx []byte) (code uint32) {
 	// check format
 	parts := bytes.Split(tx, []byte("="))
@@ -64,7 +65,7 @@ func (app *KVStoreApplication) isValid(tx []byte) (code uint32) {
 	return code
 }
 
-// DeliverTx .
+// DeliverTx 交易送达响应 .
 func (app *KVStoreApplication) DeliverTx(req abcitypes.RequestDeliverTx) abcitypes.ResponseDeliverTx {
 	code := app.isValid(req.Tx)
 	if code != 0 {
@@ -82,19 +83,19 @@ func (app *KVStoreApplication) DeliverTx(req abcitypes.RequestDeliverTx) abcityp
 	return abcitypes.ResponseDeliverTx{Code: 0}
 }
 
-// CheckTx .
+// CheckTx 交易校验响应 .
 func (app *KVStoreApplication) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx {
 	code := app.isValid(req.Tx)
 	return abcitypes.ResponseCheckTx{Code: code, GasWanted: 1}
 }
 
-// Commit .
+// Commit 交易提交响应 .
 func (app *KVStoreApplication) Commit() abcitypes.ResponseCommit {
 	app.currentBatch.Commit()
 	return abcitypes.ResponseCommit{Data: []byte{}}
 }
 
-// Query .
+// Query 查询交易 .
 func (app *KVStoreApplication) Query(reqQuery abcitypes.RequestQuery) (resQuery abcitypes.ResponseQuery) {
 	resQuery.Key = reqQuery.Data
 	err := app.db.View(func(txn *badger.Txn) error {
@@ -119,7 +120,7 @@ func (app *KVStoreApplication) Query(reqQuery abcitypes.RequestQuery) (resQuery 
 	return
 }
 
-// InitChain .
+// InitChain 初始化链 .
 func (KVStoreApplication) InitChain(req abcitypes.RequestInitChain) abcitypes.ResponseInitChain {
 	return abcitypes.ResponseInitChain{}
 }
--- example/kvstore/go.mod
+++ example/kvstore/go.mod
@@ -1,3 +1,8 @@
 module github.com/codechain/example/kvstore
 
 go 1.14
+
+require (
+	github.com/dgraph-io/badger v1.6.0
+	github.com/tendermint/tendermint v0.33.1
+)
