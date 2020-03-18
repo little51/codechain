49c78a225a8816184de7bfebd6830e0612e1a65a
little51 <little51@126.com>
2020-03-18 12:48:06 +0800

    modify install shell

--- release/v0.0.1/codechain_v0.0.1_linux_amd64.sh
+++ release/v0.0.1/codechain_v0.0.1_linux_amd64.sh
@@ -1,3 +1,3 @@
-﻿#!/bin/sh
-wget https://github.com/little51/codechain/releases/download/V0.0.1/codechain_v0.0.1_linux_amd64.tar.gz -O codechain_v0.0.1_linux_amd64.tar.gz
-tar -xvf codechain_v0.0.1_linux_amd64.tar.gz
+﻿wget https://github.com/little51/codechain/releases/download/V0.0.1/codechain_v0.0.1_linux_amd64.tar.gz -O codechain_v0.0.1_linux_amd64.tar.gz
+mkdir codechain_v0.0.1
+tar -xvf codechain_v0.0.1_linux_amd64.tar.gz -C ./codechain_v0.0.1
