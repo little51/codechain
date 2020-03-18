e6f93cc91c6f743ed26de7166bcd180a1f2f1827
little51 <little51@126.com>
2020-03-18 12:19:21 +0800

    modify codechain_v0.0.1_linux_amd64.sh

--- release/v0.0.1/codechain_v0.0.1_linux_amd64.sh
+++ release/v0.0.1/codechain_v0.0.1_linux_amd64.sh
@@ -1,3 +1,3 @@
-﻿#!/usr/bin/env bash
+﻿#!/bin/sh
 curl -o https://github.com/little51/codechain/raw/master/release/v0.0.1/codechain_v0.0.1_linux_amd64.tar.gz
 tar -xvf codechain_v0.0.1_linux_amd64.tar.gz
